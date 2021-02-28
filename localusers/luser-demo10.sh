bb2295987e985df59b9787557bcd7617c96fb97f
eduard <mecozma@gmail.com>
2021-02-28 17:56:40 +0000

    Backup a file.

--- localusers/luser-demo10.sh
+++ localusers/luser-demo10.sh
@@ -25,6 +25,36 @@ message() {
   logger -t luser-demo10.sh "${MESSAGE}"
 }
 
+backup_file() {
+  # This function creates a backupf of a file. Returns non-zero status on error.
+  local FILE="${1}"
+
+  # Make sure the file exists.
+  if [[ -f "${FILE}" ]]
+  then
+    local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+    message "Backing up ${FILE} to ${BACKUP_FILE}."
+
+  # The exit status of the function will be the status of the cp command. 
+    cp -p ${FILE} ${BACKUP_FILE}
+  else
+    # The file does not exist, so return a non-zero exit status.
+    return 1
+  fi
+}
+
 readonly VERBOSE='true'
 message 'Hello!'
 message 'This is fun!'
+
+backup_file '/etc/passwd'
+
+
+# Make a decision based on the exit status of the function.
+if [[ "${?}" -eq '0' ]]
+then
+  message 'File backup succeeded!'
+else
+  message 'File backup failes!'
+  exit 1
+fi
